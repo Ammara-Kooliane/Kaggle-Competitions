e12f9fdcd99d40157d174b5c008090ca4a2748d8
Ammara Ith <kooliane@gmx.com>
2016-02-03 11:51:53 +0100

    Update 3_Conditional_Inference_trees.R

--- Predict_Titanic_survivors/3_Conditional_Inference_trees.R
+++ Predict_Titanic_survivors/3_Conditional_Inference_trees.R
@@ -1,7 +1,6 @@
 # Titanic competition script using the conditional inference trees
 
 library(party)
-library(rpart)
 
 # Source our engineered data
 source("1_Feature_engineering.R")
@@ -16,7 +15,7 @@ fit <- cforest(as.factor(Survived) ~ Pclass + Sex + Age + SibSp + Parch + Fare +
                data = train, controls=cforest_unbiased(ntree=1000, mtry=3))
 Prediction <- predict(fit, test, OOB=TRUE, type = "response")
 
-# Creaete the submitting file
+# Creating the submitting file
 submit <- data.frame(PassengerId = test$PassengerId, Survived = Prediction)
 write.csv(submit, file = "thirdforest.csv", row.names = FALSE)
 # Precision score of 0.81340 which is my current best
